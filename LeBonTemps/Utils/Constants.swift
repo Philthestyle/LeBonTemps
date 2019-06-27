167665c779d66435775531aaed4e4c692ca6c509
Faustin Veyssiere <faustin.veyssiere@gmail.com>
2019-06-27 07:25:28 +0200

    WeatherService created. Taking into acccount that API call will have to take 'latitude' and 'longitude' as params I had to consider it.

---
+++ LeBonTemps/Utils/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  LeBonTemps
+//
+//  Created by Faustin Veyssiere on 27/06/2019.
+//  Copyright © 2019 Faustin Veyssiere. All rights reserved.
+//
+
+import Foundation
+import CocoaLumberjack
+import KeychainAccess
+
+class Constants {
+    // MARK: - Variables
+    // Private variables
+    
+    // Public variables
+    static var apiServerURL = "https://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=ABpfSFIsBiRWewQzAnQDKgVtBzJeKFRzUS0GZQpvVCkFblU0D29dO1Q6A34PIFVjWHVQMwE6ADABalAoWykCYwBqXzNSOQZhVjkEYQItAygFKwdmXn5Uc1E7BmMKeVQ2BW5VNQ9yXT5UOwNlDyFVYFhjUDUBIQAnAWNQMlswAmAAZV86UjEGbFYxBG4CLQMoBTMHM15gVGhRNgYwCm9UPwVhVTYPbV1qVGoDZw8hVWhYaFA4ATcAOQFgUDdbMAJ%2BAHxfQlJCBnlWeQQkAmcDcQUrBzJeP1Q4&_c=cea1844ab375c1d77e932f70d92b095e"
+    
+    
+}
